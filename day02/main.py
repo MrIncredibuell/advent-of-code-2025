a6d3d3fdb4adaf362d4f72f05c7a346f3fee48b9
Joseph Buell <jlrbuellv@gmail.com>
2025-12-01 23:17:53 -0600

    Day 02 Complete

---
+++ day02/main.py
@@ -0,0 +1,36 @@
+data = [x.split("-") for x in open("input.txt").read().split(",")]
+data = [(int(a), int(b)) for a, b in data]
+
+def part1(data):
+    total = 0
+    for start, end in data:
+        i = start
+        while i <= end:
+            digits = str(i)
+            l = len(digits)
+            m = l // 2
+            if digits[:m] == digits[m:]:
+                total += i
+            i += 1
+
+    return total
+
+def part2(data):
+    total = 0
+    for start, end in data:
+        i = start
+        while i <= end:
+            digits = str(i)
+            l = len(digits)
+            for j in range(1, l):
+                if int(digits[:j] * (l // j)) == i:
+                    # print(i)
+                    total += i
+                    break
+
+            i += 1
+
+    return total
+
+# print(part1(data))
+print(part2(data))
